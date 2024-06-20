814af1dd41a282b45d3840a010f310f3093f6bf4
Camillo Cardoso Borges <camillo.cborges@bomgado.com>
2024-06-20 14:43:40 -0300

    Included IsoDateOnlyConverter

---
+++ Src/Newtonsoft.Json/Converters/DateOnlyConverterBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Newtonsoft.Json.Converters
+{
+#if (NET6_0_OR_GREATER)
+    /// <summary>
+    /// Provides a base class for converting a <see cref="DateOnly"/> to and from JSON.
+    /// </summary>
+    public abstract class DateOnlyConverterBase : JsonConverter
+    {
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>
+        /// 	<c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type objectType)
+        {
+            if (objectType == typeof(DateOnly) || objectType == typeof(DateOnly?))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+#endif
+}
